9e5b07d55a4e65971109e0684eb85f720041dbc4
unknown <dkvz@naver.com>
2023-06-28 16:18:59 +0900

    for 해봄

--- tap.js
+++ tap.js
@@ -4,27 +4,32 @@
 //  - 설명 태그에 show 없애기
 //  - 누른 버튼에 show 붙이기
 
-$(".tab-button").eq(0).on("click", 실행1);
-$(".tab-button").eq(1).on("click", 실행2);
-$(".tab-button").eq(2).on("click", 실행3);
+let j= $(".tab-button").length;
 
-function 실행1() {
-  $(".tab-button").removeClass("orange");
-  $(".tab-button").eq(0).addClass("orange");
-  $(".tab-content").removeClass("show");
-  $(".tab-content").eq(0).addClass("show");
-}
+for (let i =0; i<j; i++) {
+  $(".tab-button").eq(i).on("click", function (){;
+    $(".tab-button").removeClass("orange");
+    $(".tab-button").eq(i).addClass("orange");
+    $(".tab-content").removeClass("show");
+    $(".tab-content").eq(i).addClass("show");
+  }
 
-function 실행2() {
-  $(".tab-button").removeClass("orange");
-  $(".tab-button").eq(1).addClass("orange");
-  $(".tab-content").removeClass("show");
-  $(".tab-content").eq(1).addClass("show");
-}
+  )};
 
-function 실행3() {
-  $(".tab-button").removeClass("orange");
-  $(".tab-button").eq(2).addClass("orange");
-  $(".tab-content").removeClass("show");
-  $(".tab-content").eq(2).addClass("show");
-}
+// $(".tab-button").eq(1).on("click", function () {
+//   $(".tab-button").removeClass("orange");
+//   $(".tab-button").eq(1).addClass("orange");
+//   $(".tab-content").removeClass("show");
+//   $(".tab-content").eq(1).addClass("show");
+// }
+// );
+// $(".tab-button").eq(2).on("click", function () {
+//   $(".tab-button").removeClass("orange");
+//   $(".tab-button").eq(2).addClass("orange");
+//   $(".tab-content").removeClass("show");
+//   $(".tab-content").eq(2).addClass("show");
+// }
+// );
+
+
+
