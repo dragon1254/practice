803f2a04e4c6b022e21c0b391dcbed948f8eaca3
dragon1254 <67757961+dragon1254@users.noreply.github.com>
2023-06-29 20:18:28 +0900

    dataset까지

--- tap.js
+++ tap.js
@@ -4,32 +4,21 @@
 //  - 설명 태그에 show 없애기
 //  - 누른 버튼에 show 붙이기
 
-let j= $(".tab-button").length;
-
-for (let i =0; i<j; i++) {
-  $(".tab-button").eq(i).on("click", function (){;
-    $(".tab-button").removeClass("orange");
-    $(".tab-button").eq(i).addClass("orange");
-    $(".tab-content").removeClass("show");
-    $(".tab-content").eq(i).addClass("show");
-  }
-
-  )};
-
-// $(".tab-button").eq(1).on("click", function () {
-//   $(".tab-button").removeClass("orange");
-//   $(".tab-button").eq(1).addClass("orange");
-//   $(".tab-content").removeClass("show");
-//   $(".tab-content").eq(1).addClass("show");
+// let j = $(".tab-button").length;
+// for (let i = 0; i < j; i++) {
+//   $(".tab-button")
+//     .eq(i)
+//     .on("click", function () {
+//       탭열기(i);
+//     });
 // }
-// );
-// $(".tab-button").eq(2).on("click", function () {
-//   $(".tab-button").removeClass("orange");
-//   $(".tab-button").eq(2).addClass("orange");
-//   $(".tab-content").removeClass("show");
-//   $(".tab-content").eq(2).addClass("show");
-// }
-// );
+$(".list").click(function (e) {
+  탭열기(e.target.dataset.id);
+});
 
-
-
+function 탭열기(구멍) {
+  $(".tab-button").removeClass("orange");
+  $(".tab-button").eq(구멍).addClass("orange");
+  $(".tab-content").removeClass("show");
+  $(".tab-content").eq(구멍).addClass("show");
+}
