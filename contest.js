4ed186e39be3fef313d7adac4547a62c7adfa9cf
dragon1254 <67757961+dragon1254@users.noreply.github.com>
2023-07-31 17:18:43 +0900

    기능대회 예제1

--- contest.js
+++ contest.js
@@ -1,16 +1,27 @@
-$.get("store.json").done(function(data){
-    console.log(data);
-})
+var product =[]
 
-$(document).ready(function(){
-    data.forEach((a,i) => {
-        
-    
+$.get("./store.json").then(function(data){
+    product = data.products
+    product.forEach((a,i) => {
     var temp = `<div class="card-column">
-    <img src="${data[i].photo}">
-    <h4 class="tt">${data[i].title}</h4>
-    <p>${data[i].brand}</p>
-    <h5>가격: ${data[i].price}</h5>
+    <img src="${product[i].photo}">
+    <h4 class="tt">${product[i].title}</h4>
+    <p>${product[i].brand}</p>
+    <h5>가격: ${product[i].price}</h5>
 </div>`
     $(".row").append(temp)
+    console.log(i);
 })});
+
+$(".form-control").on("input",function(){
+    
+    // 그 상자 안의 값이 상품의 타이틀에 포함되어 있으면 현재의 리스트를 지우고 그 상품만 나타내기
+    // 포함된 글자를 노란색으로 표시하기
+// 포함된 글자를 찾아서 변수 안에 넣고
+    // var search1 = $(".form-control").on()
+// 현재 리스트를 지우고
+    // $(".card-column").html("")
+// 포함된 글자의 타이틀을 표시
+    // $(".card-column").append(``)
+// 만약 검색 상자가 비어있으면 원래 리스트 표시
+})
